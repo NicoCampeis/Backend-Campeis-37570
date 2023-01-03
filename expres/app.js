1ab96856c1b07bdcaca35acd3e9f0c3f96fae30d
nicolas <nico.campeis@hotmail.com.ar>
2023-01-03 19:05:30 -0300

    revisar codigo, no funcion el json

--- expres/app.js
+++ expres/app.js
@@ -1,20 +1,26 @@
 
 const express = require ('express')
-const ProductManager = require('../index')
+const {ProductManager} = require('./index')
 const app = express()
-const productManager = new ProductManager('./products.json')
+const productoManager = new ProductManager()
 const PORT = 8080
 
 
-app.get ('/products', async (req,res)=>{
-const products = await productManager.getProducts();
+
+
+app.get('/productos', async (req,res)=>{
+
+const products = await productoManager.getProducts();
 const {limit} = req.query
 if (limit) return res.json(products.slice(0,limit))
 else return res.json(products)
+
+
+
 })
 
-app.get ('/products/:pid', async (req,res)=>{
-    const products = await productManager.getProducts();
+app.get('/productos/:pid', async (req,res)=>{
+    const products = await productoManager.getProducts();
     const {pid} = req.params
     const product = products.find (products => products.id === pid)
 
