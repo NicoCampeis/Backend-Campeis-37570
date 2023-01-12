5779ad6b856fc88bd6cdfdd714e2fac214fe4e65
nicolas <nico.campeis@hotmail.com.ar>
2023-01-12 19:34:00 -0300

    desafio no comenzado

--- PrimerEntrega/index.js
+++ PrimerEntrega/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const { ProductManager } = require("./productManager");
 const producManager = new ProductManager
 const app = express();
-const cartsRouter = require('../routers/cartsRouter');
+
 const PORT = 8080;
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/",require("../routers/cartsRouter"))
+app.use("/api",require("../routers/cartsRouter"))
 
 
 app.listen(PORT, () => {
