ae6715b7a9765e63abd7dce1e9bdfbc81133c33a
nicolas <nico.campeis@hotmail.com.ar>
2023-01-17 11:12:27 -0300

    empeze con motores de plantilla, dejo la entrega para super corregir en algun momento, quiero ver si puedo entender este tema.

--- PrimerEntrega/index.js
+++ PrimerEntrega/index.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const app = express();
-const { ProductManager } = require("./productManager");
-const producManager = new ProductManager
+//ME DICE QUE EXPRESS.PRODUCTMANAGER NO ES UNA FUNCION EN PRODCTS.ROUTER
+//const { ProductManager } = require("./productManager");
+//const producManager = new ProductManager
+
 //requeri products y carts.
 const productsRouter = require('../routes/products.router')
 const cartsRouter = require('../routes/carts.router')
 const PORT = 8080;
 
+//instale handlebars y ahora agego lo basico para poder usarlo.
+
+const handlebars = require ('express-handlebars');
+
+app.engine('handlebars', handlebars.engine());
+
+//en la linea 15 configuramos que motor de plantilla estamos usando (handlebars)
+app.set('view engine', 'handlebars');
+
+// en la linea 17 nos va a traer las vista del directorio views
+app.set('views',__dirname +'/views');
+
+//le indicamos que plantilla vamos a mostrar 
+
+app.get("/", (req,res)=>{
+        res.status(200).render('home')
+        //seguir mirando desde los 48'
+});
 
 //middlewares
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
